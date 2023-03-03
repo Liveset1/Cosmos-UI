8a92f2058eb3a61373dc210b6983d8bdc1d4c54b
yousi <yousi@DESKTOP-UBDBL4M>
2023-03-03 16:59:44 -0500

    Add project files.

---
+++ Cosmos-UI/src/framework.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
+#include "pch.h"
+
+// Base class for UI elements
+class CSUIElement
+{
+
+};
